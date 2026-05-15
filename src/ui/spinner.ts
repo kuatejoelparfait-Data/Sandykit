0a0b4dd883b17357b3232c225a0416722b0eba9a
Joel Parfait <joelparfait237@gmail.com>
2026-05-15 23:53:30 +0200

    chore: remove v1 code, prepare for v2

--- src/ui/spinner.ts
+++
@@ -1,9 +0,0 @@
-import * as p from '@clack/prompts';
-
-export function startSpinner(msg: string): { stop: (finalMsg?: string) => void } {
-  const s = p.spinner();
-  s.start(msg);
-  return {
-    stop: (finalMsg?: string) => s.stop(finalMsg ?? msg),
-  };
-}
